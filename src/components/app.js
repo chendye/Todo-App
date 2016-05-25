cee32022230fafa8abf77983ab76ae1222f7c7cf
wenbin <wenbin@striking.ly>
2016-05-25 11:15:53 +0800

    crete app entry

--- src/components/app.js
+++ src/components/app.js
@@ -1,3 +1,33 @@
 /**
  * Created by onlycrazy on 16/5/24.
  */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CreateTodo from './CreateTodo'
+import TodoList from './TodoList'
+import Toolbar from './ToolBar'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos : []
+        }
+    }
+
+    render() {
+        let {todos} = this.state;
+        return (
+            <section class="todo-container">
+                <CreateTodo />
+                <TodoList todos = {todos} />
+                <Toolbar />
+            </section>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
